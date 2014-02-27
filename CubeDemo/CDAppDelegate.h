61ea4534243623a73dd52274218c157d43337431
esperado <fxu1990@gmail.com>
2014-02-27 14:25:29 +0800

    Initial Commit

---
+++ CubeDemo/CDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CDAppDelegate.h
+//  CubeDemo
+//
+//  Created by esperado on 14-2-27.
+//  Copyright (c) 2014年 AttackOnDobby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CubeDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CubeDemo
+//
+//  Created by esperado on 14-2-27.
+//  Copyright (c) 2014年 AttackOnDobby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CDAppDelegate class]));
+    }
+}
